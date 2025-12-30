355021d933de3ed4147d749c8f4d613f64063db2
CharlesLee <lichaoyu330@gmail.com>
2025-12-30 16:44:48 +0800

    转换DESUtil加密类

--- core/common/src/main/java/cn/thecover/media/core/common/util/DESUtil.kt
+++ core/common/src/main/java/cn/thecover/media/core/common/util/DESUtil.kt
@@ -1,38 +1,41 @@
-package cn.thecover.media.core.common.util;
+package cn.thecover.media.core.common.util
 
-import javax.crypto.Cipher;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.DESKeySpec;
-import java.security.Key;
-import java.security.SecureRandom;
+import java.security.Key
+import java.security.SecureRandom
+import java.util.Locale
+import javax.crypto.Cipher
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.DESKeySpec
 
 /**
  * des工具
  *
  */
-public class DESUtil {
+object DESUtil {
     /**
      * 密钥算法
      */
-    private static final String ALGORITHM = "DES";
+    private const val ALGORITHM = "DES"
+
     /**
      * 加密/解密算法-工作模式-填充模式
      */
-    private static final String CIPHER_ALGORITHM = "DES/ECB/PKCS5Padding";
+    private const val CIPHER_ALGORITHM = "DES/ECB/PKCS5Padding"
+
     /**
      * 默认编码
      */
-    private static final String CHARSET = "utf-8";
+    private const val CHARSET = "utf-8"
 
     /**
      * 签名算法
      */
-    public static final String SIGN_ALGORITHMS = "SHA1PRNG";
+    const val SIGN_ALGORITHMS: String = "SHA1PRNG"
 
     /**
      * 简单key
      */
-    public static final String SIMPLE_KEY = "682864A5";
+    const val SIMPLE_KEY: String = "682864A5"
 
     /**
      * 生成key
@@ -41,10 +44,11 @@ public class DESUtil {
      * @return
      * @throws Exception
      */
-    private static Key generateKey(String password) throws Exception {
-        DESKeySpec dks = new DESKeySpec(password.getBytes(CHARSET));
-        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(ALGORITHM);
-        return keyFactory.generateSecret(dks);
+    @Throws(Exception::class)
+    private fun generateKey(password: String): Key? {
+        val dks = DESKeySpec(password.toByteArray(charset(CHARSET)))
+        val keyFactory = SecretKeyFactory.getInstance(ALGORITHM)
+        return keyFactory.generateSecret(dks)
     }
 
 
@@ -55,24 +59,24 @@ public class DESUtil {
      * @param data 待加密字符串
      * @return 加密后内容
      */
-    public static String encrypt(String key, String data) {
-        if (key== null || key.length() < 8) {
-            throw new RuntimeException("加密失败，key不能小于8位");
+    fun encrypt(key: String, data: String?): String? {
+        if (key.length < 8) {
+            throw RuntimeException("加密失败，key不能小于8位")
         }
-        if (data == null){
-            return null;
+        if (data == null) {
+            return null
         }
         try {
-            Key secretKey = generateKey(key);
-            Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
-            SecureRandom random = SecureRandom.getInstance(SIGN_ALGORITHMS);
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey, random);
-            byte[] bytes = cipher.doFinal(data.getBytes(CHARSET));
-            return parseByte2HexStr(bytes);
-        } catch (Exception e) {
-            e.printStackTrace();
+            val secretKey = generateKey(key)
+            val cipher = Cipher.getInstance(CIPHER_ALGORITHM)
+            val random = SecureRandom.getInstance(SIGN_ALGORITHMS)
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, random)
+            val bytes = cipher.doFinal(data.toByteArray(charset(CHARSET)))
+            return parseByte2HexStr(bytes)
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
-        return null;
+        return null
     }
 
     /**
@@ -82,83 +86,69 @@ public class DESUtil {
      * @param data 待解密字符串
      * @return 解密后内容
      */
-    public static String decrypt(String key, String data) {
-        if (key== null || key.length() < 8) {
-            throw new RuntimeException("加密失败，key不能小于8位");
+    fun decrypt(key: String, data: String?): String? {
+        if (key.length < 8) {
+            throw RuntimeException("加密失败，key不能小于8位")
         }
-        if (data == null){
-            return null;
+        if (data == null) {
+            return null
         }
         try {
-            Key secretKey = generateKey(key);
-            Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
-            SecureRandom random = SecureRandom.getInstance(SIGN_ALGORITHMS);
-            cipher.init(Cipher.DECRYPT_MODE, secretKey, random);
-            return new String(cipher.doFinal(parseHexStr2Byte(data)), CHARSET);
-        } catch (Exception e) {
-//            e.printStackTrace();
+            val secretKey = generateKey(key)
+            val cipher = Cipher.getInstance(CIPHER_ALGORITHM)
+            val random = SecureRandom.getInstance(SIGN_ALGORITHMS)
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, random)
+            return String(cipher.doFinal(parseHexStr2Byte(data)), charset(CHARSET))
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
-        return null;
+        return null
     }
 
     /**
      * 将二进制转换成16进制
      */
-    public static String parseByte2HexStr(byte[] buf) {
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < buf.length; i++) {
-            String hex = Integer.toHexString(buf[i] & 0xFF);
-            if (hex.length() == 1) {
-                hex = '0' + hex;
+    fun parseByte2HexStr(buf: ByteArray): String {
+        val sb = StringBuffer()
+        for (i in buf.indices) {
+            var hex = Integer.toHexString(buf[i].toInt() and 0xFF)
+            if (hex.length == 1) {
+                hex = '0'.toString() + hex
             }
-            sb.append(hex.toUpperCase());
+            sb.append(hex.uppercase(Locale.getDefault()))
         }
-        return sb.toString();
+        return sb.toString()
     }
 
     /**
      * 将16进制转换为二进制
      */
-    public static byte[] parseHexStr2Byte(String hexStr) {
-        if (hexStr==null||hexStr.length() < 1) {
-            return null;
+    fun parseHexStr2Byte(hexStr: String?): ByteArray? {
+        if (hexStr == null || hexStr.isEmpty()) {
+            return null
         }
-        byte[] result = new byte[hexStr.length() / 2];
-        for (int i = 0; i < hexStr.length() / 2; i++) {
-            int high = Integer.parseInt(hexStr.substring(i * 2, i * 2 + 1), 16);
-            int low = Integer.parseInt(hexStr.substring(i * 2 + 1, i * 2 + 2), 16);
-            result[i] = (byte) (high * 16 + low);
+        val result = ByteArray(hexStr.length / 2)
+        for (i in 0..<hexStr.length / 2) {
+            val high = hexStr.substring(i * 2, i * 2 + 1).toInt(16)
+            val low = hexStr.substring(i * 2 + 1, i * 2 + 2).toInt(16)
+            result[i] = (high * 16 + low).toByte()
         }
-        return result;
+        return result
     }
 
     /**
      * 简单加密
      * @return
      */
-    public static String simpleEncrypt(String data){
-        return encrypt(SIMPLE_KEY,data);
+    fun simpleEncrypt(data: String?): String? {
+        return encrypt(SIMPLE_KEY, data)
     }
 
     /**
      * 简单解密
      * @return
      */
-    public static String simpleDecrypt(String data){
-        return decrypt(SIMPLE_KEY,data);
-    }
-
-    public static void main(String[] args) {
-        String key="12345678";
-        String msg="111111";
-        String res="cc2739c64fefb199";
-//        System.out.println(DESUtil.encrypt(key,msg));
-//        System.out.println(DESUtil.decrypt(key,res));
-//        String s = simpleEncrypt(msg);
-//        System.out.println(s);
-//        System.out.println(simpleDecrypt(s));
-
-        System.out.println(simpleDecrypt("5098098c32b3867c"));
-        System.err.println(simpleEncrypt("15813733781"));
+    fun simpleDecrypt(data: String?): String? {
+        return decrypt(SIMPLE_KEY, data)
     }
 }
